914549c0b7c669b634b470a06d9efd4402dd253f
anilpathuri <anil.pathuri@walkingtree.tech>
2017-07-09 00:20:57 +0530

    done the it works page, sign up contact us page.

--- src/pages/contact-us/contact-us.ts
+++ src/pages/contact-us/contact-us.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'contact-us',
-  templateUrl: 'contact-us.html'
+  templateUrl: 'contact-us.html',
+  styleUrls: ['contact-us.scss']
 })
 export class ContactUsComponent {
     public title: any = "Contact Us";
--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sign-up',
-  templateUrl: 'sign-up.html'
+  templateUrl: 'sign-up.html',
+  styleUrls: ['sign-up.scss']
 })
 export class SignUpComponent {
     public title: any = "Sign up";
