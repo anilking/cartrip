906c3ea98c51651cbd8a3ef4fe02c57ec4263380
anilpathuri <anil.pathuri@walkingtree.tech>
2017-07-11 08:12:40 +0530

    added the video

--- src/pages/welcome-to/welcome-to.ts
+++ src/pages/welcome-to/welcome-to.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'welcome-to',
-  templateUrl: 'welcome-to.html'
+  templateUrl: 'welcome-to.html',
+  styleUrls:['welcome-to.scss']
 })
 export class WelcomeToComponent {
     public title: any = "Welcome To";
