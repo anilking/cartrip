5e95d196af3b9ad631b567cb50ec2412d157165a
anilpathuri <anil.pathuri@walkingtree.tech>
2017-08-01 23:22:10 +0530

    done trams.

--- src/pages/terms-conditions/terms-conditions.ts
+++ src/pages/terms-conditions/terms-conditions.ts
@@ -10,9 +10,18 @@ export class TermsConditionsComponent {
     public title: any = "Terms Conditions";
 
     public scrollElement(id) {
-    let element = document.getElementById(id);
-    window.scrollTo(0, element.offsetTop - 70);
-  }
+      let element = document.getElementById(id);
+      window.scrollTo(0, element.offsetTop - 70);
+    }
+
+    toggleIcon(e) {
+      if (e.target.children[0].className == "more-less glyphicon glyphicon-plus") {
+        e.target.children[0].className = "more-less glyphicon glyphicon-minus";
+      }
+      else {
+        e.target.children[0].className = "more-less glyphicon glyphicon-plus";
+      }
+    }
 }
 
 
