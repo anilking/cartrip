7f2bbecb436edec884a018b2bfd7d4fba90665dc
Anil K <kanil4224@gmail.com>
2017-08-05 12:34:34 +0530

    Form validations and index for become qooley

--- src/pages/it-works/it-works.ts
+++ src/pages/it-works/it-works.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'it-works',
-  templateUrl: 'it-works.html'
+  templateUrl: 'it-works.html',
+  styleUrls:['it-works.scss']
 })
 export class ItWorksComponent {
     public title: any = "How It Works";
+
+     public scrollElement(id) {
+      let element = document.getElementById(id);
+      window.scrollTo(0, element.offsetTop - (-400));
+    }
 }
 
 
