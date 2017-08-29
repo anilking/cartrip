ab003c90964a48435645c7da0c97e86388d07956
Anil K <kanil4224@gmail.com>
2017-08-30 00:17:28 +0530

    Added contact us form submission

--- src/pages/contact-us/contact-us.ts
+++ src/pages/contact-us/contact-us.ts
@@ -1,3 +1,4 @@
+import { AppService } from './../../providers/services/app.service';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import { Toastr } from './../../providers/services/toastr.service';
@@ -9,9 +10,10 @@ import { Toastr } from './../../providers/services/toastr.service';
 })
 export class ContactUsComponent {
   public title: any = 'Contact Us';
+  public successMsg: string;
   contactForm: FormGroup;
   regExp: any = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
-  constructor(private formBuilder: FormBuilder, public toastr: Toastr) {
+  constructor(private formBuilder: FormBuilder, public toastr: Toastr, private appService: AppService) {
     this.contactForm = formBuilder.group({
       'Name': [null, Validators.required],
       'Comment': [null, Validators.required],
@@ -19,8 +21,40 @@ export class ContactUsComponent {
     });
   }
 
-  onSubmit(){
-    this.contactForm.reset({});
+  onSubmit() {
+
+    let action: string = '/contactus';
+    let toastOptions: any = {
+      type: 'error',
+      title: 'Alert',
+      position: 'toast-top-right',
+      url: false
+    };
+
+    let params: any = {
+      "name": this.contactForm.value['Name'],
+      "email": this.contactForm.value['Email'],
+      "comment": this.contactForm.value['Comment']
+    };
+
+    this.appService.makeRequestWithAction(params, action)
+      .subscribe(
+      data => {
+        if (data.message.indexOf('Thank') > -1) {
+          this.successMsg = data.message;
+          this.contactForm.reset({});
+        } else {
+          //Display Toast
+          toastOptions.body = data.error;
+          this.toastr.showToastr(toastOptions);
+        }
+      },
+      error => {
+        //Display Toast
+        toastOptions.body = error.message;
+        this.toastr.showToastr(toastOptions);
+      }
+      );
   }
 }
 
--- src/providers/services/app.service.ts
+++ src/providers/services/app.service.ts
@@ -1,18 +1,59 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+// Observable class extensions
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AppService {
-    constructor() {
-        
-    }
 
-    //to show the loader
-    show() {
-      document.getElementById("loader-wrapper").style.display = "block";
-    };
+  headers: Headers;
+  options: RequestOptions;
+  public url = 'http://34.210.64.146/api/v1';
 
-    //to hide the loader
-   hide() {
-      document.getElementById("loader-wrapper").style.display = "none";
-    };
+  constructor(private http: Http) {
+    this.headers = new Headers({ 'Content-Type': 'application/json' });
+    this.options = new RequestOptions({ headers: this.headers });
+  }
+
+  //to show the loader
+  show() {
+    document.getElementById("loader-wrapper").style.display = "block";
+  };
+
+  //to hide the loader
+  hide() {
+    document.getElementById("loader-wrapper").style.display = "none";
+  };
+
+  makeRequestWithAction(params, action): Observable<any> {
+    let body = JSON.stringify(params);
+    return this.http
+      .post(this.url + action, body, this.options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || {};
+  }
+
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
